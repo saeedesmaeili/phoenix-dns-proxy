5a082b5c4b4b8dd47861121dcfcf9033d9d4f746
SEsmaeili <SEsmaeili@mafpars.com>
2025-08-17 14:55:37 +0330

    add sfdcdigital.com

--- setup.sh
+++ setup.sh
@@ -47,6 +47,7 @@ EXACT_FQDNS=(
 
 # Wildcards for your Minecraft auth stack (covers previous specific hosts):
 WILDCARD_ZONES=(
+  "sfdcdigital.com"
   "salesforce.com"
   "firebase.google.com"
   "amplitude.com"
